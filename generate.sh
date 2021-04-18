8fcc5e6991c4128cde871b2c1a8b112a6ec5406f
mb21 <mb21@users.noreply.github.com>
2021-04-18 10:26:51 +0200

    remove toolbar

--- generate.sh
+++ generate.sh
@@ -8,7 +8,6 @@ cp -r ../panwriter/try .
 pandoc --metadata mainfont='-apple-system, BlinkMacSystemFont, Segoe UI, sans-serif' --metadata monobackgroundcolor=#f0f0f0 -s \
   -M pagetitle='PanWriter' \
   --css styles.css -M document-css=true \
-  --include-before-body toolbar.html\
   --include-after-body scripts.html \
   ../panwriter/src/website.md  --output index.html
 
