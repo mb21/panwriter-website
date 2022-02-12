c45ef2b023c4575b3062e7d918639002a9d1f723
mb21 <mb21@users.noreply.github.com>
2022-02-12 11:16:12 +0100

    add MANUAL

---
+++ download.md
@@ -0,0 +1,13 @@
+# PanWriter
+
+Markdown editor for macOS, Windows and Linux.
+
+::: download
+
+- [Download](https://github.com/mb21/panwriter/releases/){#download}
+- [Try online](/try/){#try}
+
+:::
+
+You also have to [install pandoc](https://pandoc.org/installing.html) to export to most formats.
+
--- generate.sh
+++ generate.sh
@@ -6,12 +6,53 @@ cd -
 rm -r try
 cp -r ../panwriter/try .
 
-pandoc -M mainfont='-apple-system, BlinkMacSystemFont, Segoe UI, sans-serif' -M monobackgroundcolor=#f0f0f0 -s \
-  -M pagetitle='PanWriter' \
+FONT='-apple-system, BlinkMacSystemFont, Segoe UI, sans-serif'
+INCLUDES='<link rel="icon" type="image/png" href="https://github.com/mb21/panwriter/raw/master/icons/icon.png" />
+<script data-goatcounter="https://mb21.goatcounter.com/count" async src="//gc.zgo.at/count.js"></script>'
+INDEXCSS='<style>
+#download {
+  font-weight: bold;
+}
+.download ul {
+  padding-left: 0;
+}
+.download ul > li {
+  list-style: none;
+  margin: 1em 1em 1em 0;
+  display: inline-block;
+}
+.download ul > li > a {
+  border: 1px solid grey;
+  padding: 10px 15px;
+  border-radius: 5px;
+  text-decoration: none;
+}
+.download ul > li > a:hover {
+  opacity: 0.75;
+}
+@media (max-width: 600px) {
+  .download ul > li:nth-child(2) {
+    display: none;
+  }
+}
+</style>'
+
+
+pandoc -M pagetitle='PanWriter' \
+  -M mainfont="$FONT" -M monobackgroundcolor=#f0f0f0 -s \
   --css styles.css -M document-css=true \
-  -V header-includes='<link rel="icon" type="image/png" href="https://github.com/mb21/panwriter/raw/master/icons/icon.png" />' \
-  -V header-includes='<script data-goatcounter="https://mb21.goatcounter.com/count" async src="//gc.zgo.at/count.js"></script>' \
+  -V header-includes="$INCLUDES" \
+  -V header-includes="$INDEXCSS" \
   --include-after-body scripts.html \
-  ../panwriter/src/website.md  --output index.html
+  --include-before-body nav.html \
+  download.md ../panwriter/src/website.md  --output index.html
+
+pandoc --toc \
+  -M mainfont="$FONT" -M monobackgroundcolor=#f0f0f0 -s \
+  --css styles.css -M document-css=true \
+  -V header-includes="$INCLUDES" \
+  -V toc-title="Table of Contents"\
+  --include-before-body nav.html \
+  ../panwriter/MANUAL.md  --output MANUAL.html
 
 python -m SimpleHTTPServer 8000 .
